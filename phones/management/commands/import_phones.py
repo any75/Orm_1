fe758f03b9b1219227bb58c8381d9b5e0c2dbb8f
any75 <128735824+any75@users.noreply.github.com>
2024-10-29 18:52:44 +0300

    Update import_phones.py

--- phones/management/commands/import_phones.py
+++ phones/management/commands/import_phones.py
@@ -1,6 +1,6 @@
 import csv
 import os
-from slugify import slugify
+from datetime import datetime 
 from phones.models import Phone
 from django.core.management.base import BaseCommand
 class Command(BaseCommand):
@@ -12,19 +12,20 @@ class Command(BaseCommand):
             csv_dict = csv.DictReader(f, delimiter=';')
           
             for i in csv_dict:
-                a = slugify(i['name'])
+             
                 b = Phone(
                     id = i['id'],
                     name = i['name'],
-                    price = i['price'],
+                    price = round(float(i['price']), 2),
                     image = i['image'],
-                    release_date = i['release_date'],
-                    lte_exists = i['lte_exists'],
-                    slug = a
+                    release_date = datetime.strptime(i['release_date'], "%Y-%m-%d").date(),
+                    lte_exists = i['lte_exists'] == "True"
+                    
                 )
-                print(a)
-                print(b)
+           
                 b.save()
 
+        
+        
         
         
