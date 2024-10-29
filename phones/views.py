cc90b0bcfa9e989c12f0889e64166fb35c90ce22
any75 <128735824+any75@users.noreply.github.com>
2024-10-29 18:51:16 +0300

    Update views.py

--- phones/views.py
+++ phones/views.py
@@ -4,6 +4,8 @@ def catalogue(request):
     sort_by = request.GET.get("sort", "name")
     if sort_by == "min_price":
         phones = Phone.objects.all().order_by("price")
+    elif sort_by == "max_price":
+        phones = Phone.objects.all().order_by("-price")
     else:
         phones = Phone.objects.all().order_by("name")
     return render(request, "catalogue.html", {"phones": phones})
@@ -11,4 +13,6 @@ def catalogue(request):
 def phone_detail(request, slug):
     phone = get_object_or_404(Phone, slug = slug)
     return render(request, "phone_detail.html", {"phone": phone} )
+
+
 
